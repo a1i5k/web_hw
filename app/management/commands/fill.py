273416ba15486cf25c3c1d595f0d363604d3ec74
Zhuravlev Nikolay <kolyansp22222@mail.ru>
2021-05-31 22:57:19 +0300

    remake base(add extends), add requirement, remake command fill

--- app/management/commands/fill.py
+++ app/management/commands/fill.py
@@ -7,8 +7,6 @@ from app.models import *
 
 
 class Command(BaseCommand):
-    help = 'Closes the specified poll for voting'
-
     def handle(self, *args, **options):
         for i in range(11):
             tag_text = set()
@@ -19,6 +17,37 @@ class Command(BaseCommand):
             for k in tag_text:
                 tags.append(Tag(text=k))
             Tag.objects.bulk_create(tags)
+
+        for i in range(10001):
+            User.objects.create(username=Faker().simple_profile()['username'] + Faker().pystr()[:10],
+                                email=Faker().email(),
+                                password=Faker().name())
+            print(i)
+
+        for i in range(100001):
+            question = Question.objects.create(
+                author=User.objects.get(pk=randint(User.objects.first().id, User.objects.last().id)),
+                title=Faker().paragraph(nb_sentences=1),
+                text=Faker().text,
+                date=Faker().date,
+                rating=randint(-100, 100),
+                tag=Tag.objects.get(pk=randint(Tag.objects.first().id, Tag.objects.last().id))
+            )
+            tags = []
+            for j in range(5):
+                tags.append(Tag.objects.get(pk=randint(Tag.objects.first().id, Tag.objects.last().id)))
+            question.tag.set(tags)
+            print(i)
+
+        for i in range(1000001):
+            Answer.objects.create(body=Faker().text,
+                                  author=User.objects.get(pk=randint(User.objects.first().id, User.objects.last().id)),
+                                  date=Faker().date,
+                                  question=Question.objects.get(
+                                      pk=randint(Question.objects.first().id, Question.objects.last().id)),
+                                  rating=randint(-100, 100),
+                                  )
+            print(i)
 
         for i in range(100000):
             obj = Question.objects.get(pk=randint(2, 10000))
@@ -38,30 +67,4 @@ class Command(BaseCommand):
                 rating=1,
                 object_id=obj.id
             )
-            print(i)
-
-        for i in range(10001):
-            User.objects.create(username=Faker().simple_profile()['username'] + Faker().pystr()[:10],
-                                email=Faker().email(),
-                                password=Faker().name())
-            print(i)
-
-        for i in range(100001):
-            Question.objects.create(
-                author=User.objects.get(pk=randint(User.objects.first().id, User.objects.last().id)),
-                title=Faker().paragraph(nb_sentences=1),
-                text=Faker().text,
-                date=Faker().date,
-                rating=randint(-100, 100),
-            )
-            print(i)
-
-        for i in range(1000001):
-            Answer.objects.create(body=Faker().text,
-                                  author=User.objects.get(pk=randint(User.objects.first().id, User.objects.last().id)),
-                                  date=Faker().date,
-                                  question=Question.objects.get(
-                                      pk=randint(Question.objects.first().id, Question.objects.last().id)),
-                                  rating=randint(-100, 100),
-                                  )
             print(i)
